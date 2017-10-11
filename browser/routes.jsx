079c28ca25f15e3c9999d4e5b7616dc750ff4922
Natasha Desai <tashadesai@gmail.com>
2017-10-11 15:41:43 -0400

    initial commit

---
+++ browser/routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+import Main from './components/Main.jsx';
+
+
+const Routes = (props) => (
+  <Router history={history}>
+    <div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </main>
+    </div>
+  </Router>
+);
+
+export default Routes;
